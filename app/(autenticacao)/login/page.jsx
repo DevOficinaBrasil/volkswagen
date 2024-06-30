f452883f2e8a776d375a8d16f1b8f0da0787370b
Gabriel Lopes Pereira <54251267+lopix320@users.noreply.github.com>
2024-06-30 20:26:15 -0300

    fix/ cria hooks para fazer redirect

--- app/(autenticacao)/login/page.jsx
+++ app/(autenticacao)/login/page.jsx
@@ -1,87 +1,103 @@
-"use client"
+"use client";
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Avatar from '@mui/material/Avatar';
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Alert } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import VolksButton from '@/app/components/defaultButton';
-import redirectPage from '@/src/redirects/redirect/redirect';
-import generalRedirectPage from '@/src/redirects/generalRedirect/redirect';
-import { deleteTokens } from '../handler';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Alert } from "@mui/material";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import VolksButton from "@/app/components/defaultButton";
+import { deleteTokens } from "../handler";
+import useRedirectPage from "@/app/hooks/useRedirect";
 const defaultTheme = createTheme();
 
-export default function SignIn(){
-  const [alert, setAlert] = React.useState(null)
-  const router = useRouter()
+export default function SignIn() {
+  const [alert, setAlert] = React.useState(null);
+  const router = useRouter();
+
+  const { redirectPage, generalRedirectPage } = useRedirectPage();
 
   React.useEffect(() => {
     const deleteOldTokens = async () => {
-      deleteTokens()
-    }
+      deleteTokens();
+    };
 
-    deleteOldTokens()
-  }, [])
+    deleteOldTokens();
+  }, []);
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    setAlert(null)
+    event.preventDefault();
+    setAlert(null);
 
-    const formData = new FormData(event.currentTarget)
+    const formData = new FormData(event.currentTarget);
 
-    try{
-      const request = await fetch('/api/auth',{
-        method: 'POST',
+    try {
+      const request = await fetch("/api/auth", {
+        method: "POST",
         body: formData,
-      })
+      });
 
-      const response = await request.text()
+      const response = await request.text();
 
-      if(!request.ok){
-        throw new Error(response)
+      if (!request.ok) {
+        throw new Error(response);
       }
-
-      if(response.role == 'common'){
-        setAlert(null)
-        redirectPage()
-      }else if(response.role == 'manager'){
-        setAlert(null)
-        generalRedirectPage()
-      }else{
-        router.push('/')
+      console.log(response);
+      if (response.role == "common") {
+        setAlert(null);
+        redirectPage();
+      } else if (response.role == "manager") {
+        setAlert(null);
+        generalRedirectPage();
+      } else {
+        router.push("/");
       }
-    }catch(error){
-      setAlert(error.message)
+    } catch (error) {
+      setAlert(error.message);
     }
-  }
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
 
-        <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
-
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <Image src='https://w7.pngwing.com/pngs/528/129/png-transparent-volkswagen-2019-hd-logo.png' width={150} height={150} alt='' />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <Image
+              src="https://w7.pngwing.com/pngs/528/129/png-transparent-volkswagen-2019-hd-logo.png"
+              width={150}
+              height={150}
+              alt=""
+            />
           </Avatar>
 
           <Typography component="h1" variant="h5">
             Acessar
           </Typography>
 
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -91,7 +107,12 @@ export default function SignIn(){
               name="email"
               autoComplete="email"
               autoFocus
-              sx={{'& .MuiOutlinedInput-root': {backgroundColor: '#F8F8F8', '& fieldset': {border: 'none'},},}}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  backgroundColor: "#F8F8F8",
+                  "& fieldset": { border: "none" },
+                },
+              }}
             />
 
             <TextField
@@ -103,12 +124,20 @@ export default function SignIn(){
               type="password"
               id="password"
               autoComplete="current-password"
-              sx={{'& .MuiOutlinedInput-root': {backgroundColor: '#F8F8F8', '& fieldset': {border: 'none'},},}}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  backgroundColor: "#F8F8F8",
+                  "& fieldset": { border: "none" },
+                },
+              }}
             />
 
-            <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Lembrar senha" />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Lembrar senha"
+            />
 
-            { alert && <Alert severity="error">{alert}</Alert> }
+            {alert && <Alert severity="error">{alert}</Alert>}
 
             <VolksButton type="submit" fullWidth>
               Login
@@ -128,9 +157,8 @@ export default function SignIn(){
               </Grid>
             </Grid>
           </Box>
-
         </Box>
       </Container>
     </ThemeProvider>
-  )
+  );
 }
