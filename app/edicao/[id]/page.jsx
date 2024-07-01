9745cd7ed7b898989b4b6ba3f40ea5072d40a2a7
desev oficina <dev.kaiohenrique@gmail.com>
2024-06-30 21:00:37 -0300

    FEAT/ mais edições

--- app/edicao/[id]/page.jsx
+++ app/edicao/[id]/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import MoreEditions from "../../components/MoreEditions";
+import Title from "../../components/Title";
 const page = async (url) => {
   const data = await fetch(
     "https://apiob.oficinabrasil.com.br/Backend-jornalOficinaBrasil/server.php/api/edicao/" +
-      url.params.id,
+    url.params.id,
     {
       cache: "no-store",
     }
@@ -18,12 +19,22 @@ const page = async (url) => {
   }
 
   return (
-    <div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: edicao.url,
-        }}
-      ></div>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-12 gap-5  mt-10">
+        <div className="lg:col-span-10 col-span-12">
+          <div className="font-semibold text-volks-blue-800 text-3xl " >{edicao.Name}</div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: edicao.url,
+            }}
+          ></div>
+        </div>
+
+        <div className="lg:col-span-2 col-span-12">
+          <MoreEditions />
+        </div>
+      </div>
+
     </div>
   );
 };
