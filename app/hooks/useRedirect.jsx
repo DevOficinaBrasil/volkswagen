7dbc379914a81a12e6eb064a7184a8547b275ba4
Gabriel Lopes Pereira <54251267+lopix320@users.noreply.github.com>
2024-07-03 15:37:11 -0300

    feat/ redireciona para tela da live se usuario estier inscrito e live ative

--- app/hooks/useRedirect.jsx
+++ app/hooks/useRedirect.jsx
@@ -2,18 +2,59 @@
 
 import UserContext from "@/src/contexts/UserContext";
 import { useRouter } from "next/navigation";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function useRedirectPage() {
-  const { verify } = useContext(UserContext);
+  const { verify, userData } = useContext(UserContext);
+  const [subscribedTrainings, setSubscribedTrainingss] = useState([]);
+  const [subscribedTraining, setSubscribedTraining] = useState([]);
+  const [verifySubscribed, setVerifySubscribed] = useState(null);
+  const [trigger, setTrigger] = useState(0);
   const router = useRouter();
 
-  const redirectPage = () => {
-    verify();
+  useEffect(() => {
+    const getTrainings = async () => {
+      const request = await fetch("/api/subscribedTrainings", {
+        method: "GET",
+        cache: "no-cache",
+      });
 
-    router.push("/users/sucesso");
+      const response = await request.json();
+
+      if (request.ok) {
+        response.map((trainingData) => {
+          console.log(trainingData);
+          if (trainingData.active == "1") {
+            setSubscribedTraining(trainingData);
+            if (trainingData.on_live == 1 && trainingData.Inscrito == 1) {
+              router.push("/treinamento-ao-vivo");
+            } else {
+              router.push("/users/sucesso");
+            }
+          }
+        });
+        setSubscribedTrainingss(response);
+        setVerifySubscribed(true);
+      } else {
+        setSubscribedTrainingss(response);
+        setVerifySubscribed(false);
+      }
+    };
+
+    getTrainings();
+  }, [trigger]);
+
+  const redirectPage = () => {
+    setTrigger(1);
+    verify();
+    // console.log(subscribedTraining);
+    if (subscribedTraining.on_live == 1 && subscribedTraining.Inscrito == 1) {
+      router.push("/treinamento-ao-vivo");
+    } else {
+      router.push("/users/sucesso");
+    }
   };
-  
+
   const generalRedirectPage = () => {
     verify();
 
