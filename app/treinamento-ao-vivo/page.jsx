fb7647543f96c5fa595cde82bfd308908d013b3e
Gabriel Lopes Pereira <54251267+lopix320@users.noreply.github.com>
2024-07-04 15:38:04 -0300

    fix/ abre material em uma nova aba

--- app/treinamento-ao-vivo/page.jsx
+++ app/treinamento-ao-vivo/page.jsx
@@ -151,7 +151,7 @@ function TreinamentoAoVivo() {
               <Button
                 className="bg-volks-blue-800 rounded-lg hover:bg-volks-blue-800 hover:opacity-50 h-fit "
                 href="/documents/A importância do Óleo Certo para o motor - Maxi Performance 1.pdf"
-                download="A importância do Óleo Certo para o motor - Maxi Performance 1.pdf"
+                target="_blank"
               >
                 <h1 className="font-normal text-center uppercase text-lg text-white w-fit h-fit rounded-xl border-volks-blue-800">
                   MATERIAL TÉCNICO
