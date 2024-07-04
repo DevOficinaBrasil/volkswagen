6a23a19876b0953497c7a6179c80474eccd173a2
Kaaaioh <dev.kaiohenrique@gmail.com>
2024-07-04 11:15:38 -0300

    fix /chat css

--- app/treinamento-ao-vivo/page.jsx
+++ app/treinamento-ao-vivo/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Chat from "../components/Chat";
+import "./Chat.css";
 import { useContext } from "react";
 import UserContext from "@/src/contexts/UserContext";
 import useContextCustom from "../hooks/useContextCustom";
