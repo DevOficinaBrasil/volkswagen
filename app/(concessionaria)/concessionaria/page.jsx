6a475f97e37edd64ded569af3b2367b05b29c409
William Oliveira <will_belo1@hotmail.com>
2024-08-05 11:25:13 -0300

    Página de boas vindas

--- app/(concessionaria)/concessionaria/page.jsx
+++ app/(concessionaria)/concessionaria/page.jsx
@@ -5,12 +5,50 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Layout from './components/Layout'
+import { Box, Button, Divider, Typography } from '@mui/material';
 
 export default function Dashboard() {
+    function Title({ children }){
+        return(
+            <Box className="flex items-center justify-center mb-3">
+                <Box className="grow">
+                    <Divider sx={{ borderBottomWidth: 1, width: '95%' }} />
+                </Box>
+                <Typography variant="h3" className="relative font-bold inline-block" sx={{
+                '::before': {
+                    position: 'absolute',
+                    content: '""',
+                    right: -10,
+                    zIndex: -1,
+                    width: '72%',
+                    height: '100%',
+                    borderRadius: 1,
+                    backgroundColor: '#01B9FE',
+                }
+                }}>
+                {children}
+                </Typography>
+                <Box className="grow">
+                    <Divider sx={{ borderBottomWidth: 1, width: '80%', ml: 5 }} />
+                </Box>
+            </Box>
+        )
+    }
+
     return (
         <Layout>
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                Bem vindo
+                <Title variant='h3' className='font-bold'>Bem Vindo</Title>
+                <Box className="text-center px-28">
+                    <Typography variant='h5' gutterBottom>ao seu painel de administrador</Typography>
+                    <Typography variant='body'>
+                        Aqui você pode ver e controlar os inscritos na sua unidade, baixar os kits de divulgação e várias outras funcionalidades que ainda serão disponibilizadas em breve.
+                    </Typography>
+                </Box>
+                <Box className="flex flex-col gap-5 mt-10">
+                    <Button sx={{ backgroundColor: "transparent", border: "2px solid #00B1FF", borderRadius: "5px" }} href='/concessionaria/trainings' fullWidth>Treinamentos</Button>
+                    <Button sx={{ backgroundColor: "transparent", border: "2px solid #00B1FF", borderRadius: "5px" }} href='/concessionaria/kits' fullWidth>Kits de divulgação</Button>
+                </Box>
             </Container>
         </Layout>
     );
